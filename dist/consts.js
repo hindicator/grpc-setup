3c778bde67107361b6031282e281073604a64fea
HindicatoR <galgil39@gmail.com>
2024-07-14 11:37:09 -0400

    add googleTest utils

---
+++ dist/consts.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.INPUT_INSTALLATION_PATH = exports.INPUT_GOOGLE_TEST_VERSION = exports.INPUT_INCLUDE_GOOGLE_TEST = exports.INPUT_GRPC_VERSION = exports.INSTALLATION_CACHE_KEY = void 0;
+exports.INSTALLATION_CACHE_KEY = 'grpc-setup';
+exports.INPUT_GRPC_VERSION = 'grpc-version';
+exports.INPUT_INCLUDE_GOOGLE_TEST = 'include-google-test';
+exports.INPUT_GOOGLE_TEST_VERSION = 'google-test-version';
+exports.INPUT_INSTALLATION_PATH = 'installation-path';
